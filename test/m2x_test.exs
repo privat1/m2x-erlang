922cd802d9f1a3995e9c91f9079cc8c473f5a394
Joe Eli McIlvain <joe.eli.mac@gmail.com>
2015-04-07 17:43:47 -0700

    Add basic MockEngine for testing. (Imported from m2x-elixir and modified)

--- test/m2x_test.exs
+++ test/m2x_test.exs
@@ -1,12 +1,8 @@
 defmodule M2XTest do
   use ExUnit.Case
 
-  def api_key do
-    "0123456789abcdef0123456789abcdef"
-  end
-
-  def client do
-    :m2x.client(api_key)
+  def real_client do
+    :m2x.client("0123456789abcdef0123456789abcdef")
   end
 
   test "client user_agent" do
@@ -14,8 +10,57 @@ defmodule M2XTest do
       :m2x_client.user_agent
   end
 
-  test "client get /status" do
-    IO.inspect client.({:raw, :get, "/status"})
-    IO.inspect client.({:get, "/status"})
+  test "real_client raw get /status" do
+    {:ok, res} = real_client.({:raw, :get, "/status"})
+    assert res[:status] == 200
+    assert res[:json] == [{"api", "OK"}, {"triggers", "OK"}]
+    assert res[:raw] |> is_binary
+    assert res[:headers] |> is_list
+  end
+
+  test "real_client get /status" do
+    {:ok, status, json} = real_client.({:get, "/status"})
+    assert status == 200
+    assert json == [{"api", "OK"}, {"triggers", "OK"}]
+  end
+
+  test "client get /status (verified by hand)" do
+    subject = MockEngine.client(
+      {:get, "/status", :null},
+      {200, [{"api", "OK"}, {"triggers", "OK"}]}
+    )
+    {:ok, status, json} = subject.({:get, "/status"})
+    assert status == 200
+    assert json == [{"api", "OK"}, {"triggers", "OK"}]
+  end
+
+  test "client get /status => 200" do
+    :ok = MockEngine.verify(
+      {:get, "/status", :null},
+      {200, [{"api", "OK"}, {"triggers", "OK"}]},
+      fn(client) ->
+        client.({:get, "/status"})
+      end
+    )
+  end
+
+  test "client_error with params" do
+    :client_error = MockEngine.verify(
+      {:get, "/foo/bar", [{"baz", "baz_value"}]},
+      {404, [{"message", "not found"}]},
+      fn(client) ->
+        client.({:get, "/foo/bar", [{"baz", "baz_value"}]})
+      end
+    )
+  end
+
+  test "server_error with no return json" do
+    :server_error = MockEngine.verify(
+      {:get, "/foo/bar", :null},
+      {555, :null},
+      fn(client) ->
+        client.({:get, "/foo/bar"})
+      end
+    )
   end
 end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,108 @@
+
+defmodule MockEngine do
+  use GenServer
+
+  defmodule State do
+    defstruct \
+      ref:         nil,
+      api_key:     nil,
+      api_base:    nil,
+      api_version: nil,
+      req_verb:    nil,
+      req_path:    nil,
+      req_body:    nil,
+      res_status:  nil,
+      res_body:    nil,
+      res_headers: nil
+  end
+
+  def start do
+    GenServer.start_link(MockEngine, %State{}, name: MockEngineProcess)
+  end
+
+  def api_key do
+    "0123456789abcdef0123456789abcdef"
+  end
+
+  def api_base do
+    "http://api-m2x.att.com" # TODO: use SSL
+  end
+
+  def api_version do
+    :v2
+  end
+
+  def verify(request, response, block) when is_function(block) do
+    subject = client(request, response)
+    {is_ok, status, json} = block.(subject)
+    ^response = {status, json}
+    is_ok
+  end
+
+  def client(request, response) do
+    client = :m2x.client(
+      "0123456789abcdef0123456789abcdef",
+      api_base,
+      api_version,
+      MockEngine
+    )
+    setup(request, response)
+    client
+  end
+
+  def setup(request, response) do
+    start
+    GenServer.call MockEngineProcess, {:setup, request, response}
+  end
+
+  def request(verb, url, header_list, body, _) do
+    GenServer.call MockEngineProcess, {:request, verb, url, header_list, body}
+  end
+
+  def body(ref) do
+    GenServer.call MockEngineProcess, {:body, ref}
+  end
+
+  defp to_json(:null) do "" end
+  defp to_json(params) do :jsx.encode(params) end
+
+  def handle_call({:setup, {req_verb, req_path, req_params},
+                           {res_status, res_params}}, _, _state) do
+    {:reply, :ok, %State {
+      ref:         make_ref,
+      api_key:     api_key,
+      api_base:    api_base,
+      api_version: api_version,
+      req_verb:    req_verb,
+      req_path:    req_path,
+      req_body:    to_json(req_params),
+      res_status:  res_status,
+      res_body:    to_json(res_params),
+      res_headers: res_params==:null && [] || [{"Content-Type", "application/json"}]
+    }}
+  end
+
+  def handle_call({:request, verb, url, header_list, body}, _, state) do
+    headers = Enum.into(header_list, %{})
+    {^verb, ^url} = {state.req_verb, state.api_base <> "/" <> Atom.to_string(state.api_version) <> state.req_path}
+    case state.req_body do
+    "" -> ^body = ""
+    _  ->
+      {:ok, "application/json"} = Map.fetch(headers, "Content-Type")
+      ^body = state.req_body
+    end
+    api_key = state.api_key
+    user_agent = :m2x_client.user_agent
+    {:ok, ^api_key}    = Map.fetch(headers, "X-M2X-KEY")
+    {:ok, ^user_agent} = Map.fetch(headers, "User-Agent")
+    {:reply, {:ok, state.res_status, state.res_headers, state.ref}, state}
+  end
+
+  def handle_call({:body, given_ref}, _, state) do
+    ^given_ref = state.ref
+    {:reply, {:ok, state.res_body}, state}
+  end
+end
+
+
 ExUnit.start()
