3ddf98655ce34736e92bd6a68b4218e8aeeae1f8
Joe Eli McIlvain <joe.eli.mac@gmail.com>
2015-12-31 14:29:44 -0800

    Remove unused variable from tests.

--- test/m2x_job_test.exs
+++ test/m2x_job_test.exs
@@ -2,7 +2,6 @@ defmodule Test_m2x_job do
   use ExUnit.Case
 
   job = "00112233445566778899aabbccddeeff"
-  params = [{"param1", "value1"}, {"param2", "value2"}]
 
   for {func, arity, func_params, req_method, req_path, req_params} <- [
     { :view, "/2", :null, :get, "/jobs/"<>job, :null  },
